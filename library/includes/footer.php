5bd7b5989538f3280690b7bf1a0fa9824c10ddec
shamanthr21 <60599384+shamanthr21@users.noreply.github.com>
2021-07-30 11:17:25 +0530

    Add files via upload

---
+++ library/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2020 Online Library Management System |<a href="https://www.instagram.com/shamu_krishna21?r=nametag" target="_blank"  > Designed by : SHAMANTH R</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
